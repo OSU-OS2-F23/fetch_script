5f64bd9552b63c6fbd07befff635b14ad137b775
Rogersyp <songyip@onid.oregonstate.edu>
2023-04-05 14:53:52 -0700

    add comments to script

--- run.sh
+++ run.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+# get url of current origin
 OLD_ORIGIN=$(git config --get remote.origin.url)
 
+# set remote url to template repo
 git remote set-url origin git@github.com:OSU-OS2-S23/JOS-Labs.git
 
+# fetch all branches (include commit history)
 git fetch
 
-
+# set remote back to old origin
 git remote set-url origin $OLD_ORIGIN
 
+# individually checkout to each branch and push
 git checkout master
 git push
 
