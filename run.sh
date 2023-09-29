cb80a4efad24e0aa0b19f973fe39ccba2cfa4a76
Rogersyp <songyip@onid.oregonstate.edu>
2023-09-29 15:59:08 -0700

    update script

--- run.sh
+++ run.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+set -e # Exit on error
+
 # get url of current origin
 OLD_ORIGIN=$(git config --get remote.origin.url)
+
+if [ -z "$OLD_ORIGIN" ]; then
+  echo "Failed to get the remote origin URL. Please setup SSH Keys first!"
+  exit 1  # Exit with a non-zero status
+else
+  echo "Success. Remote origin URL: $OLD_ORIGIN"
+fi
 
 # set remote url to template repo
 git remote set-url origin git@github.com:OSU-OS2-S23/JOS-Labs.git
@@ -12,13 +21,18 @@ git fetch
 # set remote back to old origin
 git remote set-url origin $OLD_ORIGIN
 
+# set push default to simple
+git config —global push.default simple
+
 # individually checkout to each branch and push
 git checkout master
 git push
 
 
-for branch in lab1 lab2 lab3 lab4
+for branch in lab4 lab3 lab2 lab1
 do
   git checkout $branch
   git push
 done
+
+echo "End of the script."
